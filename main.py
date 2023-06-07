e9f0271dbb896cf44fdefb35c2415baf20781313
sai-238 <saiprasath238@gmail.com>
2023-06-07 23:07:46 +0530

    under development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
+import datetime
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
@@ -11,8 +12,20 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-talk('Hi Boss! This is friday at you service')
-talk('what Shall i Do ?')
+def wishMe():
+	hour = int(datetime.datetime.now().hour)
+	if hour>= 0 and hour<12:
+		talk("Good Morning Boss!")
+
+	elif hour>= 12 and hour<18:
+		talk("Good Afternoon Boss!")
+
+	else:
+		talk("Good Evening Boss!")
+wishMe()
+talk('This is friday at you service')   
+talk('what can i Do for you?')
+
 try:
     with sr.Microphone() as source:
         print('Listening...')
@@ -24,5 +37,5 @@ try:
         else:
             talk('Did you tell me something to do?')
 except:
-    talk("Boss i can't here you , are you talking to me")
+    talk("Boss! i can't here you , are you talking to me")
     pass        
